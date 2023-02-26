bbeca12fd4f48f08d637c06ef986966a943bf541
RajKKapadia <raajforyou@gmail.com>
2023-02-26 11:47:24 +0530

    explained render template

--- 26022023/app.py
+++ 26022023/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'OK'
+    return render_template('home.html')
+
+@app.route('/contactus', methods=['GET', 'POST'])
+def constact_us():
+    return render_template('contactus.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
