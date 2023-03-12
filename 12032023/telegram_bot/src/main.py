316ff22352e8a7098b58408f248217f83bccf5a6
RajKKapadia <raajforyou@gmail.com>
2023-03-12 11:15:29 +0530

    telegram openai connection success

--- 12032023/telegram_bot/src/main.py
+++ 12032023/telegram_bot/src/main.py
@@ -1,8 +1,15 @@
-import json
+import os
 
 from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
+
+from helper.telegram_api import send_message
+from helper.openai_api import text_completion
 
 app = Flask(__name__)
+
+HEADER_TOKEN = os.getenv('HEADER_TOKEN')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -10,6 +17,27 @@ def home():
 
 @app.route('/telegram', methods=['GET', 'POST'])
 def telegram():
-    body = request.get_json()
-    print(json.dumps(body))
-    return 'OK'
+    try:
+        body = request.get_json()
+        message = body['message']
+        chat = message['chat']
+        sender_id = chat['id']
+        text = message['text']
+        secret_token = request.headers['X-Telegram-Bot-Api-Secret-Token']
+
+        if HEADER_TOKEN == secret_token:
+            print('Authenticated request.')
+
+        '''
+        We need to pass this incoming message to Openai
+        Receive the response from Openai
+        Send back a reponse to the request
+        '''
+
+        response = text_completion(text)
+
+        send_message(sender_id, response)
+    except:
+        pass
+    finally:
+        return 'OK'
